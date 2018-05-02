2cb3f0a333fc4cc1eb614bf8a9d16afacc93848e
SycoYT <38894250+SycoYT@users.noreply.github.com>
2018-05-02 15:14:03 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,14 +7,14 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === '*help') {
-    	message.reply('This is a bot created and developed by xSycox the main cmd start char is (*) and these are the list of current commands every command will have a help cmd and the cmds are 1. *yomama 2. *meme 3. *randomjokes');
+    if (message.content === '^help') {
+    	message.reply('This is a bot created and developed by xSycox the main cmd start char is (^) and these are the list of current commands every command will have a help cmd and the cmds are 1. ^yomama 2. ^meme 3. ^randomjokes');
   	}
-    if (message.content === '*yomama help') {
-    	message.reply('the yomama jokes will be *yomama(1-50)');
+    if (message.content === '^yomama help') {
+    	message.reply('the yomama jokes will be ^yomama(1-50)');
   	}
-    if (message.content === '*meme help') {
-    	message.reply('the memes cmd are custom made or from internet the cmd will be *meme(1-50)');
+    if (message.content === '^meme help') {
+    	message.reply('the memes cmd are custom made or from internet the cmd will be ^meme(1-50)');
     }
     if (message.content === 'why did the chicken cross the road?') {
     	message.reply('BEACAUSE ITS CROSSY ROAD');
